76955995f924b45cc4ce3bd7aa8a4eb93b3b6902
Elias <Elias.Benfridja06@gmail.com>
2026-06-27 20:18:09 +0100

    changed file

--- Frontend/visa-tracker/src/components/ProtectedRoute.tsx
+++ Frontend/visa-tracker/src/components/ProtectedRoute.tsx
@@ -3,7 +3,8 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const access_token = localStorage.getItem("access_token");
   if (access_token) {
-    return <>{children}</> ;
-  } else { <Navigate to="/login" /> };
+    return <>{children}</>;
+  }
+  return <Navigate to="/login" />;
 }
 export default ProtectedRoute;
