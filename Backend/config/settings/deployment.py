3ce37dbf25b3d2e86a2ed32149374a99795973c5
Elias <Elias.Benfridja06@gmail.com>
2026-06-27 21:22:38 +0100

    added cors origins

--- Backend/config/settings/deployment.py
+++ Backend/config/settings/deployment.py
@@ -5,6 +5,8 @@ import dj_database_url
 DEBUG = False
 ALLOWED_HOSTS = ['application-tracker-production-3af2.up.railway.app']
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
