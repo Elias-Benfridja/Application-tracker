68a7f83be003e4027ec21da5865991bb273dc6e2
Elias <Elias.Benfridja06@gmail.com>
2026-06-28 17:48:13 +0100

    explicitly pass aws credentials to boto3

--- Backend/config/db_backends/iam_auth/base.py
+++ Backend/config/db_backends/iam_auth/base.py
@@ -1,23 +1,21 @@
 import boto3
-import logging
+import os
 from django.db.backends.postgresql import base
-
-logger = logging.getLogger(__name__)
 
 class DatabaseWrapper(base.DatabaseWrapper):
     def get_connection_params(self):
         params = super().get_connection_params()
-        try:
-            client = boto3.client('rds', region_name='eu-north-1')
-            token = client.generate_db_auth_token(
-                DBHostname=params['host'],
-                Port=int(params['port']),
-                DBUsername=params['user']
-            )
-            params['password'] = token
-            params['sslmode'] = 'require'
-            logger.info(f"IAM token generated for {params['user']}@{params['host']}")
-        except Exception as e:
-            logger.error(f"Failed to generate IAM token: {e}")
-            raise
+        client = boto3.client(
+            'rds',
+            region_name='eu-north-1',
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+        )
+        token = client.generate_db_auth_token(
+            DBHostname=params['host'],
+            Port=int(params['port']),
+            DBUsername=params['user']
+        )
+        params['password'] = token
+        params['sslmode'] = 'require'
         return params
