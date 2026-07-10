9c542a8a53762c2a2eb5ba9b20a02a3cc8c27d1b
Elias <Elias.Benfridja06@gmail.com>
2026-07-10 10:59:08 +0100

    fix base url

--- Frontend/visa-tracker/src/api/axios.ts
+++ Frontend/visa-tracker/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "application-tracker-8h92.onrender.com";
+const BASE_URL = "https://application-tracker-8h92.onrender.com";
 
 const api = axios.create({
     baseURL: BASE_URL,
